99d2ce59e7b98d4174ad7d1e522e07e19ebb1cf1
Benny Lichtner <bennlich@gmail.com>
2016-02-18 09:28:09 -0800

    replace bob-view with real skeleton

--- src/main-view.js
+++ src/main-view.js
@@ -1,37 +1,69 @@
 <main-view>
 
-  <bob-view room_ref={ roomRef } items={ items }></bob-view>
+  <div class="main-container">
+    <div class="participants"></div>
+    
+    <div class="track-list-and-submit">
+      <div class="track-list">
+        <div class="track" each={ key, track in tracks }>
+          { track.url }
+        </div>
+      </div>
 
-  this.roomRef = null;
-  this.items = [];
+      <div class="submit-track">
+        <input name="urlInput" type="text" placeholder="Enter URL" onkeypress="{ handleKeyPress }" />
+        <div class="submit-track-button" onclick="{ submitTrack }">Submit</div>
+      </div>
+    </div>
+  </div>
 
-  // our datastore
-  var dbRef = new Firebase("https://bennlich.firebaseio.com/fmn");
+  <script>
+    this.roomRef = null;
+    this.tracks = [];
 
-  joinRoom(roomName) {
-    console.log("Joining room", roomName);
-
-    // remove old listeners
-    if (this.roomRef) {
-      this.roomRef.off();
+    handleKeyPress(e) {
+      var enter_key = 13;
+      if (e.which === enter_key) {
+        this.submitTrack();
+      }
+      return true;
     }
 
-    this.roomRef = dbRef.child(roomName);
+    submitTrack() {
+      this.roomRef.child("tracks").push({
+        url: this.urlInput.value
+      });
+      this.urlInput.value = "";
+    }
 
-    // Update our model when the datastore changes
-    console.log("Listening to room", this.roomRef.toString());
-    this.roomRef.on("value", function(snap) {
-      this.items = snap.child("items").val();
-      // Force riot to re-render when the datastore changes.
-      // Turns out this is only actually needed for the initial
-      // render--subsequent renders are triggered automatically
-      // by the event handlers. Try commenting out the line below.
-      this.update();
-    }.bind(this));
-  }
+    // our datastore
+    var dbRef = new Firebase("https://bennlich.firebaseio.com/fmn");
 
-  // listen for room change events fired from
-  // the parent element
-  opts.roomEvents.on("change", this.joinRoom);
+    joinRoom(roomName) {
+      console.log("Joining room", roomName);
+
+      // remove old listeners
+      if (this.roomRef) {
+        this.roomRef.off();
+      }
+
+      this.roomRef = dbRef.child(roomName);
+
+      // Update our model when the datastore changes
+      console.log("Listening to room", this.roomRef.toString());
+      this.roomRef.on("value", function(snap) {
+        this.tracks = snap.child("tracks").val();
+        // Force riot to re-render when the datastore changes.
+        // Turns out this is only actually needed for the initial
+        // render--subsequent renders are triggered automatically
+        // by the event handlers. Try commenting out the line below.
+        this.update();
+      }.bind(this));
+    }
+
+    // listen for room change events fired from
+    // the parent element
+    opts.roomEvents.on("change", this.joinRoom);
+  </script>
 
 </main-view>
