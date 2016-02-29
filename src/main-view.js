c677618811b8639a88d3e3e174af33b6f81e8593
Benny Lichtner <bennlich@gmail.com>
2016-02-28 16:17:01 -0800

    tabs to 2 spaces

--- src/main-view.js
+++ src/main-view.js
@@ -1,18 +1,16 @@
 <main-view>
-<div class="header">
+  <div class="header">
     <div class="login">
-        <form id="fmn-login">
+      <form id="fmn-login">
 
         <input type="text" name="login" value="" placeholder="Username">
         <input type="password" name="password" value="" placeholder="Password">
         <input type="submit" value="Login">
-        </form>
+      </form>
     </div>
-</div>
+  </div>
 
-    <div class="main-container">
-
-
+  <div class="main-container">
     <div class="participants">
       <div class="participant" each={ person in participants }>
         { person.name }
