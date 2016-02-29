01b7c845a01a010266b54fe895e6e90aa9313507
Benny Lichtner <bennlich@gmail.com>
2016-02-28 21:39:52 -0800

    Add the youtube helper

---
+++ src/youtube-helper.js
@@ -0,0 +1,25 @@
+var youtubeAPIKey = "AIzaSyBIyAMsIGLWV3vx_DdoP2yKjdh92PWjrh8";
+
+function initYoutubeAPI() {
+  gapi.client.setApiKey(youtubeAPIKey);
+  gapi.client.load('youtube', 'v3');
+}
+
+window.youtubeHelper = {
+  getVideoDetailsForId: function(id, callback) {
+    if (typeof callback !== "function") {
+      callback = function(res) { console.log(res); };
+    }
+
+    var request = gapi.client.youtube.videos.list({
+      id: id,
+      fields: "items/snippet",
+      part: 'snippet'
+    });
+
+    request.execute(function(result) {
+      var details = result.items[0].snippet;
+      callback(details);
+    });
+  }
+};
