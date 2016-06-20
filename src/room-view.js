abc26bc4587589288cf48ded0e9a8583df07c7b9
Benny Lichtner <bennlich@gmail.com>
2016-06-19 23:56:34 -0700

    auto scroll the activities list!

--- src/room-view.js
+++ src/room-view.js
@@ -166,6 +166,40 @@ const RoomView = Vue.extend({
         userId: this.getPlayerId()
       });
     }
+  },
+  directives: {
+    'scrollActivities': {
+      bind: function() {
+        this.stickToBottom = true;
+
+        // don't autoscroll the activities list if the
+        // user scrolls away from the bottom
+        this.el.addEventListener("scroll", () => {
+          if (this.el.scrollTop == (this.el.scrollHeight - this.el.offsetHeight))
+            this.stickToBottom = true;
+          else
+            this.stickToBottom = false;
+        });
+      },
+      update: function(roomRef) {
+        if (roomRef) {
+          this.activitiesRef = roomRef.child("activities");
+          this.activitiesCallback = this.activitiesRef.on("child_added", () => {
+            // do autoscroll the activities list
+            // if we're in stickToBottom mode
+            if (this.stickToBottom) {
+              Vue.nextTick(() => {
+                this.el.scrollTop = this.el.scrollHeight;
+              });
+            }
+          });
+        } else if (this.activitiesRef) {
+          this.activitiesRef.off("child_added", this.activitiesCallback);
+          this.activitiesRef = null;
+          this.activitiesCallback = null;
+        }
+      }
+    }
   }
 });
 
